7a83781c7167415fa0a72b19ffcb58e141bf209d
genuinegaurav <157043114+genuinegaurav@users.noreply.github.com>
2024-12-22 22:29:34 +0530

    Add files via upload

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://xgkayxgxyjcwcfdcazuc.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhna2F5eGd4eWpjd2NmZGNhenVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ3NTgzNjksImV4cCI6MjA1MDMzNDM2OX0.7XhMPcITYbz2QazcEIpD62Xa4O-hbkhnzumdTJ9rzeE';
+}
